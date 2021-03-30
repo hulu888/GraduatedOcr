8caec61ae3e621c0c1e0a190b8af6ea743f83b28
keen <keen1@vip.qq.com>
2021-03-30 22:08:15 +0800

    ocr

--- GraduatedOcr/asgi.py
+++ GraduatedOcr/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for GraguatedOcr project.
+ASGI config for GraduatedOcr project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GraguatedOcr.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GraduatedOcr.settings')
 
 application = get_asgi_application()
